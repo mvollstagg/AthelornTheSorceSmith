2981f32085fc0f77f9e1c70a95a65dee11ce9aeb
mvollstagg <mvollstagg@gmail.com>
2023-06-14 22:45:11 +0300

    Inventory Character Stats

--- Assets/Scripts/Managers/CharacterUIManager.cs
+++ Assets/Scripts/Managers/CharacterUIManager.cs
@@ -6,6 +6,7 @@ using TMPro;
 public class CharacterUIManager : MonoBehaviour
 {
     public Transform stats;
+    public Transform statsInventoryMenu;
     private Dictionary<string, CharacterStat> characterStats;
 
     void Update()
@@ -16,6 +17,20 @@ public class CharacterUIManager : MonoBehaviour
     public void UpdateStatUI(Dictionary<string, CharacterStat> statsData)
     {
         foreach (Transform statTransform in stats)
+        {
+            string statName = statTransform.name;
+            if (statsData.TryGetValue(statName, out CharacterStat stat))
+            {
+                TextMeshProUGUI statusText = statTransform.Find("Status").GetComponent<TextMeshProUGUI>();
+                statusText.text = $"{stat.currentValue}/{stat.maxValue}";
+
+                Image barImage = statTransform.Find("Bar").GetComponent<Image>();
+                float fillAmount = (float)stat.currentValue / stat.maxValue;
+                barImage.fillAmount = Mathf.Clamp01(fillAmount);
+            }
+        }
+        // TODO: Convert this to an another UI Script which will be event based so there won't be two foreach loops
+        foreach (Transform statTransform in statsInventoryMenu)
         {
             string statName = statTransform.name;
             if (statsData.TryGetValue(statName, out CharacterStat stat))
